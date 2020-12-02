4a562f1999c0bc9a2ace1e89c65adacbf0d9fcbd
aaron <462826@qq.com>
2020-12-02 09:15:39 +0800

    add proxmox lib

---
+++ ui/src/proxmox/storage.go
@@ -0,0 +1,80 @@
+package proxmox
+
+import (
+	"fmt"
+	"net/url"
+)
+
+type Storage struct {
+	StorageType string  `json:"type"`
+	Active      float64 `json:"active"`
+	Total       float64 `json:"total"`
+	Content     string  `json:"content"`
+	Shared      float64 `json:"shared"`
+	Storage     string  `json:"storage"`
+	Used        float64 `json:"used"`
+	Avail       float64 `json:"avail"`
+	Node        Node
+}
+
+type StorageList map[string]Storage
+
+func (storage Storage) CreateVolume(FileName string, DiskSize string, VmId string) (map[string]interface{}, error) {
+	var form url.Values
+	var err error
+	var data map[string]interface{}
+	var target string
+
+	//fmt.Println("!CreateVolume")
+
+	form = url.Values{
+		"filename": {FileName},
+		//		"node":     {storage.node.Node},
+		"size":   {DiskSize},
+		"format": {"qcow2"},
+		"vmid":   {VmId},
+	}
+
+	target = "nodes/" + storage.Node.Node + "/storage/" + storage.Storage + "/content"
+	data, err = storage.Node.Proxmox.PostForm(target, form)
+	if err != nil {
+		fmt.Println("Error!!!")
+		return nil, err
+	}
+	//fmt.Println("Storage created")
+	return data, err
+}
+
+func (storage Storage) Volumes() (VolumeList, error) {
+	var err error
+	var target string
+	var data map[string]interface{}
+	var list VolumeList
+	var volume Volume
+	var results []interface{}
+
+	//fmt.Println("!Volumes")
+
+	target = "nodes/" + storage.Node.Node + "/storage/" + storage.Storage + "/content"
+	data, err = storage.Node.Proxmox.Get(target)
+	if err != nil {
+		return nil, err
+	}
+
+	list = make(VolumeList)
+	results = data["data"].([]interface{})
+	for _, v0 := range results {
+		v := v0.(map[string]interface{})
+		volume = Volume{
+			Size:    v["size"].(float64),
+			VolId:   v["volid"].(string),
+			VmId:    v["vmid"].(string),
+			Format:  v["format"].(string),
+			Content: v["content"].(string),
+			Used:    v["used"].(float64),
+			storage: storage,
+		}
+		list[volume.VolId] = volume
+	}
+	return list, nil
+}
---
+++ ui/src/proxmox/task.go
@@ -0,0 +1,65 @@
+package proxmox
+
+import (
+	"encoding/json"
+	"errors"
+	"strings"
+	"time"
+)
+
+type Task struct {
+	UPid       string  `json:"upid"`
+	Type       string  `json:"type"`
+	Status     string  `json:"status"`
+	ExitStatus string  `json:"exitstatus"`
+	PID        float64 `json:"pid"`
+	PStart     float64 `json:"pstart"`
+	StartTime  float64 `json:"starttime"`
+	EndTime    float64 `json:"endtime"`
+	ID         string  `json:"id"`
+	proxmox    ProxMox
+}
+
+type TaskList map[string]Task
+
+type TaskResult struct {
+	Data Task `json:"data"`
+}
+
+func (task Task) GetStatus() (string, string, error) {
+	var target string
+	var err error
+	var raw []byte
+	var data TaskResult
+
+	upidParts := strings.Split(task.UPid, ":")
+	target = "nodes/" + upidParts[1] + "/tasks/" + task.UPid + "/status"
+	//fmt.Println("target  " + target)
+	raw, err = task.proxmox.GetBytes(target)
+	if err != nil {
+		return "", "", err
+	}
+	err = json.Unmarshal(raw, &data)
+	if err != nil {
+		return "", "", err
+	}
+	return data.Data.Status, data.Data.ExitStatus, nil
+}
+
+func (task Task) WaitForStatus(status string, timeout int) (string, error) {
+	var i int
+	var err error
+	var actstatus string
+	var exitstatus string
+	for i = 0; i < timeout; i++ {
+		actstatus, exitstatus, err = task.GetStatus()
+		if err != nil {
+			return "", err
+		}
+		if actstatus == status {
+			return exitstatus, nil
+		}
+		time.Sleep(time.Second * 1)
+	}
+	return "", errors.New("Timeout reached")
+}
