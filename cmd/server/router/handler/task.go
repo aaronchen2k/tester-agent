1271500b425f6678e26f5d0fa64dcfec155de3fd
aaron <462826@qq.com>
2021-01-24 18:05:57 +0800

    gen test plan, task, queue and build

--- cmd/server/router/handler/task.go
+++ cmd/server/router/handler/task.go
@@ -1,10 +1,6 @@
 package handler
 
 import (
-	"fmt"
-	_domain "github.com/aaronchen2k/tester/internal/pkg/domain"
-	_logUtils "github.com/aaronchen2k/tester/internal/pkg/libs/log"
-	"github.com/aaronchen2k/tester/internal/server/model"
 	"github.com/aaronchen2k/tester/internal/server/service"
 	"github.com/kataras/iris/v12"
 )
@@ -17,17 +13,4 @@ type TaskController struct {
 
 func NewTaskController() *TaskController {
 	return &TaskController{}
-}
-func (g *TaskController) PostCreate() (result _domain.RpcResult) {
-	var task model.Task
-	if err := g.Ctx.ReadJSON(&task); err != nil {
-		_logUtils.Error(err.Error())
-		result.Fail("wrong request data")
-		return
-	}
-
-	po := g.TaskService.Save(task)
-	count := g.QueueService.GenerateFromTask(po)
-	result.Success(fmt.Sprintf("create %d queues for task %d.", count, po.ID))
-	return result
 }
